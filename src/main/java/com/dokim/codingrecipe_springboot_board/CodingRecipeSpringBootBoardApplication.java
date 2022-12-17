9d6be2431a2bae968a178e4297186c21bd93afb2
KimDohyoun79 <dogdo5726@gmail.com>
2022-12-17 17:19:07 +0900

    [ADD] first commit, setting yml

---
+++ src/main/java/com/dokim/codingrecipe_springboot_board/CodingRecipeSpringBootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.dokim.codingrecipe_springboot_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingRecipeSpringBootBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingRecipeSpringBootBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dokim/codingrecipe_springboot_board/CodingRecipeSpringBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokim.codingrecipe_springboot_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingRecipeSpringBootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
